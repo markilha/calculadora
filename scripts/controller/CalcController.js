aabd9dda869524bd599a1f341166b92252c2ca2b
Markilha <markilha@gmail.com>
2020-06-24 00:01:41 -0300

    10. C05 - Data e Hora

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,17 +1,46 @@
 class CalcController{
 
 constructor(){
+    this._locale = 'pt-BR';
+    this._displayCalcEl = document.querySelector("#display");
+    this._dateEl = document.querySelector("#data");
+    this._timeEl = document.querySelector("#hora");
+
     this._displayCalc = "0";
     this._currentDate;
     this.initialze();
 }
 initialze(){
-    let displayCalcEl = document.querySelector("#display");
-    let dateEl = document.querySelector("#data");
-    let timeEl = document.querySelector("#hora");
-    displayCalcEl.innerHTML = "12345";
-    dateEl.innerHTML = "01/02/2020";
-    timeEl.innerHTML = "00:00";
+    this.setDisplayDateTime();
+    setInterval(()=>{
+        this.setDisplayDateTime();
+    },1000 );
+
+
+}
+
+setDisplayDateTime(){
+    this.displayDate = this.currentDate.toLocaleDateString(this._locale,{
+        day: "2-digit",
+        month: "long",
+        year: "numeric"
+    })
+    this.displaytime= this.currentDate.toLocaleTimeString(this._locale)
+
+
+}
+get displaytime(){
+return this._timeEl.innerHTML;
+}
+set displaytime(value){
+    return this._timeEl.innerHTML = value;
+    }
+get displayDate(){
+    return this._dateEl.innerHTML;
+}
+
+set displayDate(value){
+    return this._dateEl.innerHTML = value;
 }
 
 get displayCalc(){
@@ -22,7 +51,7 @@ set displayCalc(value){
     this._displayCalc = value;
 }
 get currentDate(){
-    return this._currentDate;
+    return new Date();
 }
 set currentDate(value){
     this._currentDate = value;
