67f46f1bceaca23afd9ac0c1b3a02b13f342a3b6
Markilha <markilha@gmail.com>
2020-06-23 23:38:01 -0300

    9. C04 - Manipulando o DOM

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -2,21 +2,30 @@ class CalcController{
 
 constructor(){
     this._displayCalc = "0";
-    this._dataAtual;
+    this._currentDate;
+    this.initialze();
+}
+initialze(){
+    let displayCalcEl = document.querySelector("#display");
+    let dateEl = document.querySelector("#data");
+    let timeEl = document.querySelector("#hora");
+    displayCalcEl.innerHTML = "12345";
+    dateEl.innerHTML = "01/02/2020";
+    timeEl.innerHTML = "00:00";
 }
 
 get displayCalc(){
     return this._displayCalc;
 }
 
-set displaycalc(value){
+set displayCalc(value){
     this._displayCalc = value;
 }
-get _dataAtual(){
-    return this._dataAtual;
+get currentDate(){
+    return this._currentDate;
 }
-set dataAtual(value){
-    this._dataAtual = value;
+set currentDate(value){
+    this._currentDate = value;
 }
 
 
