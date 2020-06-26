ce4cc98252e03856a207d14f425a37d990eef432
Markilha <markilha@gmail.com>
2020-06-26 09:53:21 -0300

    20. C15 - Botão Ponto

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -31,6 +31,9 @@ class CalcController {
 
     clearAll() {
         this._operation = [];
+        this._lastNumber='';
+        this._operation = '';
+        
         this.setLastNumberToDisplay();
     }
 
@@ -151,11 +154,7 @@ class CalcController {
             if (this.isOperator(value)) {
 
                 this.setLastOperation(value);
-
-            } else if (isNaN(value)) {
-
-                console.log('Outra coisa', value);
-
+           
             } else {
 
                 this.pushOperator(value);
@@ -172,7 +171,7 @@ class CalcController {
 
                 let newValue = this.getLastOperation().toString() + value.toString();
 
-                this.setLastOperation(parseInt(newValue));
+                this.setLastOperation(parseFloat(newValue));
 
                 this.setLastNumberToDisplay();
 
@@ -184,6 +183,19 @@ class CalcController {
     //tratamento de erro
     setError() {
         this.displayCalc = "Error";
+    }
+
+    addDot(){
+
+         let lastOperation = this.getLastOperation();
+       
+         if(this.isOperator(lastOperation)||!lastOperation){
+             this.pushOperator('0.');
+         }else{
+             this.setLastOperation(lastOperation.toString() + '.');
+             this.setLastNumberToDisplay();
+         }
+
     }
 
     execBtn(value) {
@@ -214,7 +226,7 @@ class CalcController {
                 this.calc();
                 break;
             case 'ponto':
-                this.addOperation('.');
+                this.addDot('.');
                 break;
             case '0':
             case '1':
